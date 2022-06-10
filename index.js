248a5238c21f1b7c5bc5719e65b5e8ce587898be
kongjianqiu <872221317@qq.com>
2022-06-10 17:13:09 +0800

    commit

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * tablink index.js
+ * @author kongjianqiu
+ * @description
+ * @created 2022/6/10 16:28:33
+ */
---
+++ src/tablink.js
@@ -0,0 +1,13 @@
+/**
+ * tablink tablink.js
+ * @author kongjianqiu
+ * @description
+ * @created 2022/6/10 16:28:44
+ */
+
+class tablink {
+    constructor() {
+    }
+}
+
+export default tablink
