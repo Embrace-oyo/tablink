421afc4c465437a616ed0c3e4da129c0112cb522
kongjianqiu <872221317@qq.com>
2022-06-10 18:14:13 +0800

    commit

--- index.js
+++ index.js
@@ -4,3 +4,10 @@
  * @description
  * @created 2022/6/10 16:28:33
  */
+
+
+const Tablink = require('./src/tablink.js')
+
+let tablink = new Tablink({})
+
+tablink.Init()
--- src/tablink.js
+++ src/tablink.js
@@ -5,9 +5,24 @@
  * @created 2022/6/10 16:28:44
  */
 
-class tablink {
-    constructor() {
+function Tablink(options = {}) {
+    let defaultOptions = {
+        tabDom: null,
+        contentDom: null
+    }
+    Object.assign(defaultOptions, options)
+    let _this = this
+    let tabIndex = -1
+    this.Init = () => {
+
+    }
+    this.Listener = () => {
+
+    }
+
+    this.click = () => {
+
     }
 }
 
-export default tablink
+module.exports = Tablink
